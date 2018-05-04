fdb83dd2a2a6ca9b09e7303a57dc5f00943b1a2f
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-04 18:39:20 +0800

    one method

--- lib/default_where.rb
+++ lib/default_where.rb
@@ -38,9 +38,9 @@ module DefaultWhere
 
   def params_with_table(params = {}, options = {})
     if options.has_key?(:reject)
-      default_reject = [options[:reject]].flatten
+      default_reject = Array(options[:reject])
     elsif options.has_key?(:allow)
-      default_reject = REJECT - [options[:allow]].flatten
+      default_reject = REJECT - Array(options[:allow])
     else
       default_reject = REJECT
     end
@@ -57,46 +57,36 @@ module DefaultWhere
       value = value.strip if value.is_a?(String) && options[:strip]
       next if default_reject.include?(value)
 
-      _ref, _table, _params = key_with_table(key, value)
-      refs << _ref
-      tables << _table
-      final_params.merge! _params
+      if key =~ /\./
+        _table, _column = key.split('.')
+        _real_column = _column.split('-').first
+        _ref = reflections[_table]
+        if _ref && _ref.klass.column_names.include?(_real_column)
+          _real_table = _ref.table_name
+        elsif connection.data_sources.include?(_table) && connection.column_exists?(_table, _real_column)
+          _real_table = _table
+          keys = reflections.select { |_, v| !v.polymorphic? && v.table_name == _table }.keys
+          if keys && keys.size == 1
+            _ref = keys.first.to_sym
+          else
+            raise 'please use reflection name!'
+          end
+        else
+          return
+        end
+        final_params["#{_real_table}.#{_real_column}"] = value
+      else
+        _real_column = key.split('-').first
+        return [] unless column_names.include?(_real_column)
+        final_params["#{key}"] = value
+      end
     end
 
     [final_params, refs, tables]
   end
 
-  def key_with_table(key, value)
-    final_params = {}
-    _ref = nil
-    _table = nil
-    if key =~ /\./
-      _table, _column = key.split('.')
-      _real_column = _column.split('-').first
-      _ref = reflections[_table]
-      if _ref && _ref.klass.column_names.include?(_real_column)
-        _real_table = _ref.table_name
-      elsif connection.data_sources.include?(_table) && connection.column_exists?(_table, _real_column)
-        _real_table = _table
-        keys = reflections.select { |_, v| !v.polymorphic? && v.table_name == _table }.keys
-        if keys && keys.size == 1
-          _ref = keys.first.to_sym
-        else
-          raise 'please use reflection name!'
-        end
-      else
-        return []
-      end
-      final_params["#{_real_table}.#{_real_column}"] = value
-    else
-      _real_column = key.split('-').first
-      return [] unless column_names.include?(_real_column)
-      final_params["#{key}"] = value
-    end
-
-    [_ref, _table, final_params]
-  end
-
 end
 
-ActiveRecord::Base.extend DefaultWhere
+ActiveSupport.on_load :active_record do
+  extend DefaultWhere
+end
