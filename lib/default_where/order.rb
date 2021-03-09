0a20b2d58f2572fe6cafdaa2cc7be6a9411930c0
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-09 09:49:48 +0800

    defautl order should use hash

--- lib/default_where/order.rb
+++ lib/default_where/order.rb
@@ -2,15 +2,20 @@
 
 module DefaultWhere
   module Order
+    PATTERN = {
+      '-asc': :asc,
+      '-desc': :desc
+    }
 
     def default_where_order(params)
-      order_array = []
+      order_hash = {}
 
       params.sort_by{ |_, v| v.to_i }.each do |i|
-        order_array << i[0].sub(/-(asc|desc)$/, '-asc' => ' ASC', '-desc' => ' DESC')
+        k, v = i[0].split('-')
+        order_hash[k] = v
       end
 
-      order(order_array)
+      order(order_hash)
     end
 
     def default_where_order_filter(params)
@@ -18,6 +23,6 @@ module DefaultWhere
         k.end_with?('-asc', '-desc') && String(v) =~ /^[1-9]$/
       end
     end
-    
+
   end
 end
